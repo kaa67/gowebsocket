489d12dbd8d5be8d045fe67e753e3061c765b04b
Kolosov Andrei <kolosovaa@abdgtl.com>
2025-08-05 12:39:04 +0300

    http added

---
+++ http.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main1() {
+	http.HandleFunc("/", helloWorld)
+
+	fmt.Println("🚀 Server running at http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
